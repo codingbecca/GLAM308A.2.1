00f6fcd04305c30e99e81e064b0bd6e6423c19ab
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-02-10 08:04:18 -0800

    part four

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ================== Part 1 ================
-console.log('==== Part 1 ====')
+console.log("==== Part 1 ====");
 const adventurer = {
   name: "Robin",
   health: 10,
@@ -26,8 +26,9 @@ adventurer.roll();
 adventurer.roll();
 
 // ================== Part 2 ================
-console.log('==== Part 2 ====')
+console.log("==== Part 2 ====");
 class Character {
+  static MAX_HEALTH = 100;
   constructor(name) {
     this.name = name;
     this.health = 100;
@@ -51,36 +52,40 @@ robin.companion.roll();
 robin.companion.companion.roll();
 
 // ================== Part 3 ================
-console.log('==== Part 3 ====')
+console.log("==== Part 3 ====");
 
-class Adventurer extends Character{
-    constructor(name, role) {
-        super(name);
-        // Adventurers have specialized roles
-        this.role = role;
-        // Every adventurer starts with a bedroll and 50 gold coins
-        this.inventory.push('bedroll', '50 gold coins')
+class Adventurer extends Character {
+  static ROLES = ["Fighter", "Healer", "Wizard"];
+  constructor(name, role) {
+    super(name);
+    // Adventurers have specialized roles
+    if (Adventurer.ROLES.find((r) => r.toLowerCase() === role.toLowerCase())) {
+      this.role = role;
     }
-    // Adventurers have the ability to scout ahead of them
-    scout() {
-        console.log(`${this.name} is scouting ahead...`);
-        super.roll();
-    }
+    // Every adventurer starts with a bedroll and 50 gold coins
+    this.inventory.push("bedroll", "50 gold coins");
+  }
+  // Adventurers have the ability to scout ahead of them
+  scout() {
+    console.log(`${this.name} is scouting ahead...`);
+    super.roll();
+  }
 }
 
 class Companion extends Character {
-    constructor(name, type, companionTo){
-        super(name);
-        this.type = type;
-        this.companionTo = companionTo
-    }
-    // Companions can help their adventurers
-    help() {
-        console.log(`${this.name} is helping out ${this.companionTo.name}`);
-        super.roll();
-    }
+  constructor(name, type, companionTo) {
+    super(name);
+    this.type = type;
+    this.companionTo = companionTo;
+  }
+  // Companions can help their adventurers
+  help() {
+    console.log(`${this.name} is helping out ${this.companionTo.name}`);
+    super.roll();
+  }
 }
 
-const robin1 = new Adventurer('Robin', 'Healer' );
-const leo = new Companion('Leo', 'Cat', robin1)
-const frank = new Companion('Frank', 'Flea', leo)
+const robin1 = new Adventurer("Robin", "Healer");
+console.log(robin1);
+const leo = new Companion("Leo", "Cat", robin1);
+const frank = new Companion("Frank", "Flea", leo);
